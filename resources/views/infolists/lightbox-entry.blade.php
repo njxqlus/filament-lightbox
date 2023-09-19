690d0693b48b9413589acce45b8f0d7b3c122c53
Mikael Ahabalyants <njxqlus@gmail.com>
2023-09-19 20:42:11 +1200

    feat: init

---
+++ resources/views/infolists/lightbox-entry.blade.php
@@ -0,0 +1,48 @@
+<a
+    {{
+        $attributes
+            ->merge([
+                'id' => $getId(),
+            ], escape: false)
+            ->merge($getExtraAttributes(), escape: false)
+    }}
+    class="glightbox"
+    href="{{ $getHref() }}"
+    data-title="{{ $getTitle() }}"
+    data-description="{{ $getDescription() }}"
+    data-desc-position="{{ $getDescPosition() }}"
+    data-type="{{ $getType() }}"
+    data-effect="{{ $getEffect() }}"
+    data-width="{{ $getWidth() }}"
+    data-height="{{ $getHeight() }}"
+    data-zoomable="{{ $getZoomable() }}"
+    data-draggable="{{ $getDraggable() }}"
+    data-sizes="{{ $getSizes() }}"
+    data-srcset="{{ $getSrtSet() }}"
+>
+    {{ $getChildComponentContainer() }}
+</a>
+
+<script type="module">
+    const lightbox = GLightbox({
+        selector: @js($getSelector()),
+        skin: @js($getSkin()),
+        openEffect: @js($getOpenEffect()),
+        closeEffect: @js($getCloseEffect()),
+        slideEffect: @js($getSlideEffect()),
+        moreText: @js($getMoreText()),
+        moreLength: @js($getMoreLength()),
+        closeButton: @js($getCloseButton()),
+        touchNavigation: @js($getTouchNavigation()),
+        touchFollowAxis: @js($getTouchFollowAxis()),
+        keyBoardNavigation: @js($getKeyBoardNavigation()),
+        closeOnOutsideClick: @js($getCloseOnOutsideClick()),
+        loop: @js($getLoop()),
+        dragToleranceX: @js($getDragToleranceX()),
+        dragToleranceY: @js($getDragToleranceY()),
+        dragAutoSnap: @js($getDragAutoSnap()),
+        preload: @js($getPreload()),
+        autoplayVideos: @js($getAutoplayVideos()),
+        autofocusVideos: @js($getAutofocusVideos()),
+    });
+</script>
---
+++ src/FilamentLightboxServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Njxqlus\Filament\Components;
+
+use Filament\Support\Assets\Asset;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentLightboxServiceProvider extends PackageServiceProvider
+{
+    public static string $name = 'filament-lightbox';
+
+    public static string $viewNamespace = 'filament-lightbox';
+
+    public function configurePackage(Package $package): void
+    {
+        $package->name(static::$name);
+
+        if (file_exists($package->basePath('/../resources/views'))) {
+            $package->hasViews(static::$viewNamespace);
+        }
+    }
+
+    public function packageBooted(): void
+    {
+        FilamentAsset::register(
+            $this->getAssets(),
+            $this->getAssetPackageName()
+        );
+    }
+
+    protected function getAssetPackageName(): ?string
+    {
+        return 'njxqlus/filament-lightbox';
+    }
+
+    /**
+     * @return array<Asset>
+     */
+    protected function getAssets(): array
+    {
+        return [
+            Css::make('filament-lightbox-styles', __DIR__ . '/../resources/dist/filament-lightbox.css'),
+            Js::make('filament-lightbox-scripts', __DIR__ . '/../resources/dist/filament-lightbox.js'),
+        ];
+    }
+}
